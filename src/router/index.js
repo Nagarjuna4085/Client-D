364105cb08969f83ee5f2a5a495181d4d7f577fc
nagarjnasanam <snagarjuna2001@gmail.com>
2025-07-16 19:49:59 -0400

    employess table

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
+                    path: '/timelog/employees', // ✅ fixed
+                    name: 'employees', // ✅ fixed
+                    component: () => import('@/views/timelog/Employees.vue')
+                },
+                {
                     path: '/uikit/formlayout',
                     name: 'formlayout',
                     component: () => import('@/views/uikit/FormLayout.vue')
@@ -117,7 +122,6 @@ const router = createRouter({
             name: 'notfound',
             component: () => import('@/views/pages/NotFound.vue')
         },
-
         {
             path: '/auth/login',
             name: 'login',
