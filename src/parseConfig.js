d3ccef865bba430e09c3b59969a31c2be0aaeee3
nagarjnasanam <snagarjuna2001@gmail.com>
2025-07-20 18:58:04 -0400

    env variables

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -11,9 +11,15 @@ export async function getParse() {
         if (!Parse || typeof Parse.initialize !== 'function') {
             throw new Error('Parse module did not load correctly');
         }
+        // Use Vite environment variables
+        const appId = import.meta.env.VITE_PARSE_APP_ID;
+        const jsKey = import.meta.env.VITE_PARSE_JS_KEY;
+        const serverUrl = import.meta.env.VITE_PARSE_SERVER_URL;
 
-        Parse.initialize('kokUdaUTVuQUSluDNbprmM7ZtS9YE98pQb6ZedVG', 'z9LxYMEBTMPj0yzhIthcXPb8YGyeJL6hW7BvTkuL');
-        Parse.serverURL = 'https://parseapi.back4app.com';
+        // Parse.initialize('kokUdaUTVuQUSluDNbprmM7ZtS9YE98pQb6ZedVG', 'z9LxYMEBTMPj0yzhIthcXPb8YGyeJL6hW7BvTkuL');
+        // Parse.serverURL = 'https://parseapi.back4app.com';
+        Parse.initialize(appId, jsKey);
+        Parse.serverURL = serverUrl;
 
         ParseInstance = Parse;
     }
